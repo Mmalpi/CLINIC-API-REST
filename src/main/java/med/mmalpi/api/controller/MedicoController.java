33203239b4f8f92f7437bb8162b95a27434c673e
Mmalpi <andrewmalpica98@gmail.com>
2023-08-14 08:56:11 -0600

    Custom response

--- src/main/java/med/mmalpi/api/controller/MedicoController.java
+++ src/main/java/med/mmalpi/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.mmalpi.api.controller;
 
 import java.awt.print.Pageable;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -30,31 +32,37 @@ public class MedicoController {
 	private MedicoRepository medicoRepository;
 	
 	@PostMapping("/guardarMedico")
-	public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
-		medicoRepository.save(new Medico(datosRegistroMedico));
+	public ResponseEntity<Medico> registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico, UriComponentsBuilder uriComponentsBuilder) {
+		Medico medico = medicoRepository.save(new Medico(datosRegistroMedico));
+		//return 201 created
+		// URL where to find the resource 
+		URI url = uriComponentsBuilder.path("/medico/{id}").buildAndExpand(medico.getId()).toUri();
+		return ResponseEntity.created(url).body(medico);
 	}
 	
 	@GetMapping("/listado")
-	public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion){
+	public ResponseEntity<Page<DatosListadoMedico>> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion){
 		//return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
 		
-		//customQuery
-		return medicoRepository.findByStatusTrue(paginacion).map(DatosListadoMedico::new);
+		//customQuery							find by "custom" 
+		return ResponseEntity.ok(medicoRepository.findByStatusTrue(paginacion).map(DatosListadoMedico::new));
 	}
 	
 	@PutMapping("/actualizacion")
 	@Transactional //necesary for commit the transaction to the db for the whole method and if an error exist, a rollback happens
-	public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosAcutalizarMedico) {
+	public ResponseEntity actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosAcutalizarMedico) {
 		Medico medico = medicoRepository.getReferenceById(datosAcutalizarMedico.id());
 		medico.actualizarDatos(datosAcutalizarMedico);
+		return ResponseEntity.ok(medico);
 	}
 	
 	//logic delete
 	@DeleteMapping("/{id}") //dinamic variable
 	@Transactional //necesary for commit the transaction to the db for the whole method and if an error exist, a rollback happens
-	public void eliminarMedico(@PathVariable Long id) {
+	public ResponseEntity eliminarMedico(@PathVariable Long id) {
 		Medico medico = medicoRepository.getReferenceById(id);
 		medico.desactivarMedico();
+		return ResponseEntity.noContent().build();
 	} 
 	
 	//perma delete
@@ -65,19 +73,11 @@ public class MedicoController {
 //		medicoRepository.delete(medico);
 //	}
 	
-	 // Manejo de excepciones para datos de entrada no válidos personalizado //custom error handler
-	//ejemplo // example 
-	// "message": "nombre: must not be blank; email: must be a well-formed email address; telefono: must match \"^[0-9]{7,15}$\"; documento: must match \"\\d{4,6}\"; direccion.calle: must not be blank;"
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
-        BindingResult bindingResult = ex.getBindingResult();
-        StringBuilder errorMessage = new StringBuilder();
-
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            errorMessage.append(fieldError.getField()).append(": ").append(fieldError.getDefaultMessage()).append("; ");
-        }
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage.toString());
-    }
+	@GetMapping("/{id}") //dinamic variable
+	public ResponseEntity<Medico>  retornaMedico(@PathVariable Long id) {
+		Medico medico = medicoRepository.getReferenceById(id);
+		return ResponseEntity.ok(medico);
+	} 
+	
 }
 
